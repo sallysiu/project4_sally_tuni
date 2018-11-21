8f100bd176e5f774a35dd6c1110dff4e936ab49b
mariamosmann <rossimosmann@gmail.com>
2018-11-21 18:49:06 -0500

    Locality is an array now, trying to compare user input w/ locality :tea:

---
+++ scripts/pseudoCode.js
@@ -0,0 +1,20 @@
+//access API in a way that only retrieves restaurants that serve bubble tea 
+//store that in a variable (apiPlace)
+
+//when user enters the place in the search box the value is stored in a variable (userPlace)
+
+//using the userPlace variable grabs only restaurants in that area (there's a key called Locality with the area)
+
+//IF THERE'S BUBBLE TEA RESTAURANTS IN THAT AREA
+//return an array with bubble tea restaurants (objects)
+//display one of these restaurantes randomly
+    //restaurant name, photo, address, $, etc
+//give option to randomize again 
+//give option to do another search
+
+//IF THERE ISN'T BUBBLE TEA RESTAURANTS IN THAT AREA
+//display message saying that there isn't any restaurant
+//give option to go to a random restaurant some place else
+//give option to do another search
+
+
